83dcde7899241183ffc5a4756954721fa249c99d
ebauschatz <eaenglish113@gmail.com>
2023-01-04 09:50:51 -0600

    initial updates from presentaton feedback

--- frontend/src/components/TeamEventComponents/TeamScheduleRow/TeamScheduleRow.jsx
+++ frontend/src/components/TeamEventComponents/TeamScheduleRow/TeamScheduleRow.jsx
@@ -9,7 +9,7 @@ const TeamScheduleRow = (props) => {
         <tr className="schedule-row">
             <td className="schedule-data-element">{props.runnerLeg.race_leg.leg_number}</td>
             <td className="schedule-data-element">{props.runnerLeg.race_leg.leg_distance}</td>
-            <td className="schedule-data-element">{props.runnerLeg.runner.user.first_name}</td>
+            <td className="schedule-data-element">{props.runnerLeg.runner.user.first_name} {props.runnerLeg.runner.user.last_name}</td>
             <td className="schedule-data-element schedule-time" onClick={() => props.editStartTime(props.runnerLeg)}>{props.runnerLeg.runner_leg_start.replace("Z","").replace("T"," ")}</td>
             <td className="schedule-data-element schedule-time" onClick={() => props.editEndTime(props.runnerLeg)}>{props.runnerLeg.runner_leg_end.replace("Z","").replace("T"," ")}</td>
             <td className="schedule-data-element">
